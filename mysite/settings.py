8e6b0f7f1f2847e3d5fc5d07df5a10e98a27b542
joymacharia <joyjulydynomite@gmail.com>
2016-11-26 18:09:44 +0300

    edit the allowed hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '07@&0!ba3g$e_yqz_p#yv(n)ne^h#ctwr@!#!h4_90%v8a1481'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
